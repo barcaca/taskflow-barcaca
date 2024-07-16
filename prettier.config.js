6a09b3acf5edd54595fd6d651662a05c2ede0d33
barcaca <73846225+barcaca@users.noreply.github.com>
2024-07-16 12:47:05 -0300

    build: adiciona npm dos components

--- prettier.config.js
+++ prettier.config.js
@@ -1,4 +1,10 @@
-// prettier.config.js
+/** @type {import('prettier').Options} */
 module.exports = {
+  singleQuote: true,
+  semi: false,
+  printWidth: 140,
+  tabWidth: 2,
+  bracketSpacing: true,
+  useTabs: false,
   plugins: ['prettier-plugin-tailwindcss'],
 }
