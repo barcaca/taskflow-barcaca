c7531a80a33bea40f6753e69426a52c34130f3be
barcaca <73846225+barcaca@users.noreply.github.com>
2024-07-16 13:24:19 -0300

    chore: atualiza configurações do ESLint e Prettier

--- prettier.config.js
+++ prettier.config.js
@@ -1,10 +1,4 @@
 /** @type {import('prettier').Options} */
 module.exports = {
-  singleQuote: true,
-  semi: false,
-  printWidth: 140,
-  tabWidth: 2,
-  bracketSpacing: true,
-  useTabs: false,
   plugins: ['prettier-plugin-tailwindcss'],
 }
